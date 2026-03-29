0fdeba2598d62db095f7b36f598a38c518aea9ec
shyllenno <shyllenno@gmail.com>
2026-03-29 16:31:32 +0100

    Fix: Bind server to 0.0.0.0 and use process.env.PORT for Render deployment compatibility

--- src/server.js
+++ src/server.js
@@ -42,8 +42,8 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT ||3000,
+    host: "0.0.0.0",
   });
 
   await server.register(Vision);
